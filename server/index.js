8fdcc20f38bb18408759bfec17de06ce592f1d04
Meg Gorregattu <meghanago@gmail.com>
2022-07-27 15:19:34 -0500

    fifth commit

--- server/index.js
+++ server/index.js
@@ -9,6 +9,9 @@ const app = express()
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname, '../index.html'))
 })
+app.get('/css', (req, res) => {
+    res.sendFile(path.join(__dirname, '../styles.css'))
+})
 
 const port = process.env.PORT || 4005 
 
